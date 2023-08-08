094e8e27d62bc1de70476401b98870a033f46f1e
mwombeki <hackerlubex@protonmail.com>
2023-08-09 00:02:33 +0300

    implement JWT authentication flow

---
+++ src/handler.rs
@@ -0,0 +1,18 @@
+use crate::{
+    jwt_auth,
+    model::{LoginUserSchema, RegisterUserSchema, TokenClaims, User},
+    response::FilteredUser,
+    AppState,
+};
+use actix_web::{
+    cookie::{time::Duration as ActixWebDuration, Cookie},
+    get, post, web, HttpMessage, HttpRequest, HttpResponse, Responder,
+};
+use argon2::{
+    password_hash::{rand_core::OsRng, PasswordHash, PasswordHasher, PasswordVerifier, SaltString},
+    Argon2,
+};
+use chrono::{prelude::*, Duration};
+use jsonwebtoken::{encode, EncodingKey, Header};
+use serde_json::json;
+use sqlx::Row;
