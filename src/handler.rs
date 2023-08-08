8e14dd740972b34ae991ffce64358bca13443881
mwombeki <hackerlubex@protonmail.com>
2023-08-09 00:02:54 +0300

    commit

--- src/handler.rs
+++ src/handler.rs
@@ -16,3 +16,16 @@ use chrono::{prelude::*, Duration};
 use jsonwebtoken::{encode, EncodingKey, Header};
 use serde_json::json;
 use sqlx::Row;
+
+fn filter_user_record(user: &User) -> FilteredUser {
+    FilteredUser {
+        id: user.id.to_string(),
+        email: user.email.to_owned(),
+        name: user.name.to_owned(),
+        photo: user.photo.to_owned(),
+        role: user.role.to_owned(),
+        verified: user.verified,
+        createdAt: user.created_at.unwrap(),
+        updatedAt: user.updated_at.unwrap(),
+    }
+}
