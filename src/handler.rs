5d58af52725f159b8656690bd7b58e00d9ca1579
mwombeki <hackerlubex@protonmail.com>
2023-08-11 17:03:36 +0300

    add logout user route handler

--- src/handler.rs
+++ src/handler.rs
@@ -128,3 +128,16 @@ async fn login_user_handler(
         .cookie(cookie)
         .json(json!({"status": "success", "token": token}))
 }
+
+#[get("/auth/logout")]
+async fn logout_handler(_: jwt_auth::JwtMiddleware) -> impl Responder {
+    let cookie = Cookie::build("token", "")
+        .path("/")
+        .max_age(ActixWebDuration::new(-1, 0))
+        .http_only(true)
+        .finish();
+
+    HttpResponse::Ok()
+        .cookie(cookie)
+        .json(json!({"status": "success"}))
+}
