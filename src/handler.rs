98a3e29cf965ecd5afd133a044c14b07e10170d8
mwombeki <hackerlubex@protonmail.com>
2023-08-11 13:53:38 +0300

    add register user route hanler

--- src/handler.rs
+++ src/handler.rs
@@ -29,3 +29,51 @@ fn filter_user_record(user: &User) -> FilteredUser {
         updatedAt: user.updated_at.unwrap(),
     }
 }
+
+#[post("/auth/register")]
+async fn register_user_handler(
+    body: web::Json<RegisterUserSchema>,
+    data: web::Data<AppState>,
+) -> impl Responder {
+    let exists: bool = sqlx::query("SELECT EXISTS(SELECT 1 FROM users WHERE email = $1")
+        .bind(body.email.to_owned())
+        .fetch_one(&data.db)
+        .await
+        .unwrap()
+        .get(0);
+
+    if exists {
+        return HttpResponse::Conflict().json(
+            serde_json::json!({"status": "fail", "message": "User with that email already exists"}),
+        );
+    }
+
+    let salt = SaltString::generate(&mut OsRng);
+    let hashed_password = Argon2::default()
+        .hash_password(body.password.as_bytes(), &salt)
+        .expect("Error while hashing password")
+        .to_string();
+    let query_result = sqlx::query_as!(
+        User,
+        "INSERT INTO users (name,email,password) VALUES ($1, $2, $3) RETURNING *",
+        body.name.to_string(),
+        body.email.to_string().to_lowercase(),
+        hashed_password
+    )
+    .fetch_one(&data.db)
+    .await;
+
+    match query_result {
+        Ok(user) => {
+            let user_response = serde_json::json!({"status": "success", "data": serde_json::json!({
+                "user": filter_user_record(&user)
+            })});
+
+            return HttpResponse::Ok().json(user_response);
+        }
+        Err(e) => {
+            return HttpResponse::InternalServerError()
+                .json(serde_json::json!({"status": "error", "message": format!("{:?}", e)}))
+        }
+    }
+}
