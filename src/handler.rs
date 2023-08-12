0fda157fab0ae9896f0da2de6d51eb4e8339e75b
mwombeki <hackerlubex@protonmail.com>
2023-08-12 15:52:13 +0300

    merge the route handlers

--- src/handler.rs
+++ src/handler.rs
@@ -165,3 +165,14 @@ async fn get_me_handler(
 
     HttpResponse::Ok().json(json_response)
 }
+
+pub fn config(conf: &mut web::ServiceConfig) {
+    let scope = web::scope("/api")
+        .service(health_checker_handler)
+        .service(register_user_handler)
+        .service(login_user_handler)
+        .service(logout_handler)
+        .service(get_me_handler);
+
+    conf.service(scope)
+}
